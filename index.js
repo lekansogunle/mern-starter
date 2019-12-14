a4fd1b093c474c4b361180f1374782357dbbae32
Olalekan Sogunle <sogunleolalekan@gmail.com>
2019-12-14 14:02:19 +0100

    create db connection

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+ });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+ console.log("MongoDB database connection established!");
+});
+
+
 app.get('/', (req, res) => {
   res.json({hello: 'world'})
 });
